488fa15e90c791cc237e27e5574f92ae8a8f172c
josedavidb <josedavidb6@gmail.com>
2017-10-19 14:29:29 -0400

    Leer los argumentos

---
+++ Laboratorio 3/DeepWeb.java
@@ -0,0 +1,22 @@
+public class DeepWeb{
+
+	public static void main(String[] args) {
+
+		if (args.length != 8) {
+      		System.out.println("Error en los argumentos del programa\nEjecutar:");
+      		System.out.println("java LeerEscribirArchivo <nombreArchivoEntrada> <nombreArchivoSalida>");
+      		System.exit(1);
+    	}
+    	String instancia = args[0];
+    	int origen = Integer.parseInt(args[1]);
+    	String algoritmo = args[2];
+    	int profundidad = Integer.parseInt(args[4]);
+    	String arb = args[5];
+    	String ord = args[6];
+    	String pred = args[7];
+
+    	System.out.println("la instancia es: " + instancia + " el origen es: " + origen
+    						+ " el algoritmo es: " + algoritmo + " la profundidad es: " + profundidad
+    						+ " el arbol es: " + arb + " el ord es: " + ord + " el pred es: " + pred);
+	}
+}
