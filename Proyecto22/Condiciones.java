e8ec50349e08fbebc33a549ac4ca029223335d84
josedavidb <josedavidb6@gmail.com>
2017-11-13 23:09:38 -0500

    Condiciones actualizacion

--- Proyecto22/Condiciones.java
+++ Proyecto22/Condiciones.java
@@ -18,29 +18,46 @@ public class Condiciones {
              boolean[] arregloSolucionFuerte = new boolean[iterador.size()];
              boolean esSolucion = true;
             interno: for (Vertice iterador2: iterador) {
-                if (this.grafo.gradoExterior(iterador2.getId()) > 0 && !iterador2.getFrontera() ){
-                    List<Vertice> sucesores = grafo.sucesores(iterador2.getId());
-                    for (Vertice w:sucesores ) {
-                        if (w.getPeso() > iterador2.getPeso() ){
-                            break interno;
-                        }
-                        else {
-                            arregloSolucionFuerte[contador] = true;
-                            break;
+                if (!iterador2.getFrontera() ){
+
+                    if(this.grafo.gradoExterior(iterador2.getId()) > 0  ){
+                        List<Vertice> sucesores = grafo.sucesores(iterador2.getId());
+                        for (Vertice w:sucesores ) {
+                            if (w.getPeso() < iterador2.getPeso() ){
+
+                                arregloSolucionFuerte[contador] = false;
+                                break interno;
+                            }
+                            else {
+
+                                arregloSolucionFuerte[contador] = true;
+
+                            }
                         }
                     }
+
+                    else{
+                        arregloSolucionFuerte[contador] = true;
+                    }   
+
                 }
+
+                else{
+                    arregloSolucionFuerte[contador] = false;
+                }
+
                 contador++;
             }
-             for (int i = 0; i<arregloSolucionFuerte.length; i++){
-                 if(arregloSolucionFuerte[i]!=true){
-                     esSolucion = false;
-                 }
-             }
 
-             if (esSolucion){
-                 System.out.println("SoyLa componente" + iterador +"Soy solucions :D!!!!!");
-             }
+            for (int i = 0; i<arregloSolucionFuerte.length; i++){
+                if(arregloSolucionFuerte[i]!=true){
+                    esSolucion = false;
+                }
+            }
+
+            if (esSolucion){
+                System.out.println("SoyLa componente" + iterador +"Soy solucions :D!!!!!");
+            }
         }
     }
 }
