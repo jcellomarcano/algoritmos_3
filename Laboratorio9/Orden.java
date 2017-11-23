f840fa5b547aeb6c2a7b0ec36ddc8ae7dd1571c6
josedavidb <josedavidb6@gmail.com>
2017-11-23 15:10:32 -0400

    Lectura archivo entrada

---
+++ Laboratorio9/Orden.java
@@ -0,0 +1,15 @@
+public class Orden{
+
+	public static void main(String[] args) {
+		if (args.length!=1){
+			System.out.println("Introdujo mal los argumentos");
+		}
+
+		else{
+			GrafoDirigido miGrafo;
+			miGrafo = new GrafoDirigido();
+			miGrafo.cargarGrafo(args[0]);
+			System.out.println(miGrafo.toString());
+		}
+	}
+}
